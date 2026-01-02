e2862133282f6d00c3021fba85b615a8a7210a3b
Raid Aqil Athallah <raidathallah036@gmail.com>
2026-01-02 13:11:09 +0700

    Final

--- klien/src/app/guru_kelas/layout.tsx
+++ klien/src/app/guru_kelas/layout.tsx
@@ -1,7 +1,7 @@
 // File: layout.tsx
 // Fungsi: Layout utama untuk seluruh halaman di rute guru kelas.
 //         Menyediakan struktur dasar seperti navbar, sidebar, dan konten dinamis.
-// Pembuat: Frima RIzky Lianda - NIM: 3312401016
+// Pembuat: Frima Rizky Lianda - NIM: 3312401016
 // Tanggal: 15 September 2025
 
 import type { Metadata } from "next";
